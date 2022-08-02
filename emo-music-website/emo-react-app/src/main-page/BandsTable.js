c214fee4144065fd3d90c37f5bb439f2e8dd48af
floralunit <71765648+floralunit@users.noreply.github.com>
2022-08-02 23:16:54 +0300

    сделала макет для таблицы нормальный

--- emo-music-website/emo-react-app/src/main-page/BandsTable.js
+++ emo-music-website/emo-react-app/src/main-page/BandsTable.js
@@ -4,145 +4,123 @@ import {NavLink} from "react-router-dom";
 
 export function BandsTable() {
     return (
-        <div className="about">
-            <table className="table">
+        <div className="table">
+            <table>
                 <tbody>
-                <td>
-                    <table>
-                        <tbody>
-                        <tr>
-                            <td className="tdUp">
-                                <p align="center"><b>Эмо 90-х</b></p></td>
-                        </tr>
-                        <tr>
-                            <td className="tdLow">
-                                <NavLink to={"/bobtilton"}>Bob Tilton</NavLink>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="tdLow">
-                                <NavLink to={"/i-hate-myself"}>I Hate Myself</NavLink>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="tdLow">
-                                <NavLink to={"/mineral"}>Mineral</NavLink>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="tdLow">
-                                <NavLink to={"/kolya"}>Kolya</NavLink>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="tdLow">
-                                <NavLink to={"/sleeping-for-sunrise"}>Sleeping for Sunrise</NavLink>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="transparent">Sunny Day</td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </td>
-                <td>
-                    <table>
-                        <tbody>
-                        <tr>
-                            <td className="tdUp">
-                                <p align="center"><b>Мидвест-эмо</b></p></td>
-                        </tr>
-                        <tr>
-                            <td className="tdLow">
-                                <a href="../groups/ian_dury.htm">American Football</a></td>
-                        </tr>
-                        <tr>
-                            <td className="tdLow">
-                                <a href="../groups/idols.htm">Camping in Alaska</a></td>
-                        </tr>
-                        <tr>
-                            <td className="tdLow">
-                                <a href="../groups/idols.htm">Marietta</a></td>
-                        </tr>
-                        <tr>
-                            <td className="tdLow">
-                                <a href="../groups/idols.htm">Tiny Moving Parts</a></td>
-                        </tr>
-                        <tr>
-                            <td className="transparent">Sunny Day</td>
-                        </tr>
-                        <tr>
-                            <td className="transparent">Sunny Day</td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </td>
-                <td>
-                    <table>
-                        <tbody>
-                        <tr>
-                            <td className="tdUp">
-                                <p align="center"><b>Русское эмо</b></p></td>
-                        </tr>
-                        <tr>
-                            <td className="tdLow">
-                                <a href="../groups/idols.htm">Bird Bone</a></td>
-                        </tr>
-                        <tr>
-                            <td className="tdLow">
-                                <a href="../groups/ian_dury.htm">botanichesky sad</a></td>
-                        </tr>
-                        <tr>
-                            <td className="tdLow">
-                                <a href="../groups/idols.htm">hay king</a></td>
-                        </tr>
-                        <tr>
-                            <td className="tdLow">
-                                <a href="../groups/idols.htm">состояние птиц</a></td>
-                        </tr>
-                        <tr>
-                            <td className="tdLow">
-                                <a href="../groups/idols.htm">это</a></td>
-                        </tr>
-                        <tr>
-                            <td className="tdLow">
-                                <a href="../groups/idols.htm">флорист</a></td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </td>
-                <td>
-                    <table>
-                        <tbody>
-                        <tr>
-                            <td className="tdUp">
-                                <p align="center"><b>Остальное</b></p></td>
-                        </tr>
-                        <tr>
-                            <td className="tdLow">
-                                <a href="../groups/idols.htm">Reasons</a></td>
-                        </tr>
-                        <tr>
-                            <td className="tdLow">
-                                <a href="../groups/idols.htm">Oakwood</a></td>
-                        </tr>
-                        <tr>
-                            <td className="transparent">Sunny Day</td>
-                        </tr>
-                        <tr>
-                            <td className="transparent">Sunny Day</td>
-                        </tr>
-                        <tr>
-                            <td className="transparent">Sunny Day</td>
-                        </tr>
-                        <tr>
-                            <td className="transparent">Sunny Day</td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </td>
+                <tr>
+                    <td className="tdUp">
+                        <p align="center"><b>Эмо 90-х</b></p></td>
+                </tr>
+                <tr>
+                    <td className="tdLow">
+                        <NavLink to={"/bobtilton"}>Bob Tilton</NavLink>
+                    </td>
+                </tr>
+                <tr>
+                    <td className="tdLow">
+                        <NavLink to={"/i-hate-myself"}>I Hate Myself</NavLink>
+                    </td>
+                </tr>
+                <tr>
+                    <td className="tdLow">
+                        <NavLink to={"/mineral"}>Mineral</NavLink>
+                    </td>
+                </tr>
+                <tr>
+                    <td className="tdLow">
+                        <NavLink to={"/kolya"}>Kolya</NavLink>
+                    </td>
+                </tr>
+                <tr>
+                    <td className="tdLow">
+                        <NavLink to={"/sleeping-for-sunrise"}>Sleeping for Sunrise</NavLink>
+                    </td>
+                </tr>
                 </tbody>
-
+            </table>
+            <table>
+                <tbody>
+                <tr>
+                    <td className="tdUp">
+                        <p align="center"><b>Мидвест-эмо</b></p></td>
+                </tr>
+                <tr>
+                    <td className="tdLow">
+                        <NavLink to={"/american-football"}>American Football</NavLink>
+                    </td>
+                </tr>
+                <tr>
+                    <td className="tdLow">
+                        <NavLink to={"/camping-in-alaska"}>Camping in Alaska</NavLink>
+                    </td>
+                </tr>
+                <tr>
+                    <td className="tdLow">
+                        <NavLink to={"/marietta"}>Marietta</NavLink>
+                    </td>
+                </tr>
+                <tr>
+                    <td className="tdLow">
+                        <NavLink to={"/tiny-moving-parts"}>Tiny Moving Parts</NavLink>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            <table>
+                <tbody>
+                <tr>
+                    <td className="tdUp">
+                        <p align="center"><b>Русское эмо</b></p></td>
+                </tr>
+                <tr>
+                    <td className="tdLow">
+                        <NavLink to={"/bird-bone"}>Bird Bone</NavLink>
+                    </td>
+                </tr>
+                <tr>
+                    <td className="tdLow">
+                        <NavLink to={"/botanichesky-sad"}>botanichesky sad</NavLink>
+                    </td>
+                </tr>
+                <tr>
+                    <td className="tdLow">
+                        <NavLink to={"/hay-king"}>hay king</NavLink>
+                    </td>
+                </tr>
+                <tr>
+                    <td className="tdLow">
+                        <NavLink to={"/sostoyanie-ptits"}>состояние птиц</NavLink>
+                    </td>
+                </tr>
+                <tr>
+                    <td className="tdLow">
+                        <NavLink to={"/eto"}>это</NavLink>
+                    </td>
+                </tr>
+                <tr>
+                    <td className="tdLow">
+                        <NavLink to={"/florist"}>флорист</NavLink>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            <table>
+                <tbody>
+                <tr>
+                    <td className="tdUp">
+                        <p align="center"><b>Остальное</b></p></td>
+                </tr>
+                <tr>
+                    <td className="tdLow">
+                        <NavLink to={"/reasons"}>Reasons</NavLink>
+                    </td>
+                </tr>
+                <tr>
+                    <td className="tdLow">
+                        <NavLink to={"/oakwood"}>Oakwood</NavLink>
+                    </td>
+                </tr>
+                </tbody>
             </table>
         </div>
     );
