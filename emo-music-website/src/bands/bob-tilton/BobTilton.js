dcc540e8e304ab9354ef2a1824d4040c123f3b5f
floralunit <71765648+floralunit@users.noreply.github.com>
2022-08-07 23:52:22 +0300

    починила папку с проектом

---
+++ emo-music-website/src/bands/bob-tilton/BobTilton.js
@@ -0,0 +1,268 @@
+import React, {useState} from 'react'
+import AwesomeSlider from 'react-awesome-slider';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
+import 'react-awesome-slider/dist/styles.css';
+import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
+import {FiChevronDown} from 'react-icons/fi';
+import {FiChevronUp} from 'react-icons/fi';
+import {SongLyrics} from "../../components/SongLyrics"
+import songsData from './songs-lyrics.json';
+
+
+export function BobTilton() {
+
+    const [selectedSong, setSelectedSong] = useState(null);
+
+    const [toggleState, setToggleState] = useState(1);
+    const toggleTab = (index) => {
+        setToggleState(index);
+    };
+
+    const [isShownSongs1, setIsShownSongs1] = useState(false);
+    const [open1, setOpen1] = useState(false);
+    const handleClickShowSongs1 = () => {
+        setIsShownSongs1(current => !current);
+        setOpen1(open => !open);
+    };
+    const [isShownSongs2, setIsShownSongs2] = useState(false);
+    const [open2, setOpen2] = useState(false);
+    const handleClickShowSongs2 = () => {
+        setIsShownSongs2(current => !current);
+        setOpen2(open => !open);
+    };
+    const [isShownSongs3, setIsShownSongs3] = useState(false);
+    const [open3, setOpen3] = useState(false);
+    const handleClickShowSongs3 = () => {
+        setIsShownSongs3(current => !current);
+        setOpen3(open => !open);
+    };
+    const [isShownSongs4, setIsShownSongs4] = useState(false);
+    const [open4, setOpen4] = useState(false);
+    const handleClickShowSongs4 = () => {
+        setIsShownSongs4(current => !current);
+        setOpen4(open => !open);
+    };
+
+    const AutoplaySlider = withAutoplay(AwesomeSlider);
+
+    return (
+        <div className="bands">
+            <a href="/">
+                <div>Вернуться назад</div>
+            </a>
+            <div className="header">
+                <h id="head">Bob Tilton</h>
+                <br/>
+                <hr/>
+                <b> Жанры: </b> emo, emo 90s, post-hardcore, british <br/>
+                <b> Годы активности: </b> 1993 – 1999 (6 лет) <br/>
+                <b> Место основания: </b> Nottingham, England <br/>
+                <b> Лейбл: </b> Subjugation records <br/>
+            </div>
+            <div className="data">
+                <div className={"empty"}/>
+                <div className="tab-bar radius">
+                    <button className={toggleState === 1 ? "tab selected" : "tab"} onClick={() => toggleTab(1)}
+                       data-text="О группе">
+                        <span className="icon">О группе</span>
+                    </button>
+                    <button className={toggleState === 2 ? "tab selected" : "tab"} onClick={() => toggleTab(2)}
+                       data-text="Дискография">
+                        <span className="icon">Дискография</span>
+                    </button>
+                    <button className={toggleState === 3 ? "tab selected" : "tab"} onClick={() => toggleTab(3)}
+                       data-text="Видосы">
+                        <span className="icon">Видосы</span>
+                    </button>
+                </div>
+                <div className={"empty"}/>
+                <div className={"blackback"}>
+                    <div className="content-tabs">
+                        <div
+                            className={toggleState === 1 ? "content  active-content" : "content"}
+                        >
+                            <div className={"Parent"}>
+                                <div className={"child1"}>
+                                    <div>
+                                        <h3> Состав </h3>
+                                        <ol className="rectangle">
+                                            <li> <img src={require('../instruments-images/singer.png')} style={{width: '25px'}} alt={''}/> Simon Feirn</li>
+                                            <li> <img src={require('../instruments-images/guitar.png')} style={{width: '30px'}} alt={''}/> Neil Johnson</li>
+                                            <li> <img src={require('../instruments-images/guitar.png')} style={{width: '30px'}} alt={''}/>(1994-1998) Chay Lawrence</li>
+                                            <li> <img src={require('../instruments-images/guitar.png')} style={{width: '30px'}} alt={''}/>(1994-1998) Ralph Hamilton</li>
+                                            <li> <img src={require('../instruments-images/bass-guitar.png')} style={{width: '30px'}} alt={''}/> Mark Simms</li>
+                                            <li> <img src={require('../instruments-images/drums.png')} style={{width: '30px'}} alt={''}/> Alan Gainey</li>
+                                        </ol>
+                                    </div>
+                                </div>
+                                <div className={"child2"}>
+                                    <AutoplaySlider
+                                        play={true}
+                                        cancelOnInteraction={true} // should stop playing on user interaction
+                                        interval={2500}
+                                        animation="cubeAnimation"
+                                    >
+                                        <div data-src={require('./images/bobtilton1.jpg')}/>
+                                        <div data-src={require('./images/bobtilton2.jpg')}/>
+                                        <div data-src={require('./images/bobtilton3.jpg')}/>
+                                        <div data-src={require('./images/bobtilton4.jpg')}/>
+                                        <div data-src={require('./images/bobtilton5.jpg')}/>
+                                    </AutoplaySlider>
+                                </div>
+                            </div>
+                            <h3> Биография </h3>
+                            Легенды Британской сцены Боб Тилтон (названы в честь американского телепроповедника и бывшего пастора семейной церкви "Слово веры"), начинали в 1993, в городе Ноттингем, еще до того как
+                            термин
+                            "имо", закрепился
+                            на Туманном Альбионе. В том же году, группа записала песни для дебютного 7" EP, который был
+                            издан через год, лейблом
+                            Subjugation records тиражем 1000 копий.
+                            <br/><br/>
+                            Два следующих года для группы сложились вполне удачно, у Bob Tilton выходит вторая по счету
+                            семёрка, в 1996 известный персонаж Джон Пилл пригласил ребят записать сессию для своего шоу на BBC Radio One.
+                            Несколько месяцев спустя Subjugation издали долгожданный и самый хитовый лонгплей группы.
+                            <br/><br/>
+                            На самом деле 96'ой был очень особенным временем, внутри независимой британской сцены, люди
+                            просто сходили с ума на
+                            концертах, начиная от Spy Versus Spy и кончая Mogwai. Конечно же Bob Tilton были не
+                            исключением, лонгплей просто разлетелся среди фанатов эмо и панка. На протяжении еще трёх лет группа рубилась на концертах по всей Британии,
+                            вернувшись домой в декабре 1998 они записали свой второй альбом на лейбле Sousaphon Recordings,
+                            под названием The Leading Hotels Of The World, вскоре после этого, барабанщик Алан Гейни переехал в Лондон, что послужило одной из причин распада группы весной 1999 года.
+                            <br/><br/>
+                            После Боб Тилтон, вокалист Саймон Фирн пел в группе Wolves Of Greece, остальные участники
+                            замутили проект I Am Spartacus.
+                            Гитарист Нейл Джонсон так же играл в Wolves Of Greece, сейчас он играет в группах avoy Grand
+                            и Andy Clambake & The Resurrection Men.
+                            <br/><br/>
+                            <div>
+                                Информация взята с <a href="https://www.last.fm/music/Bob+Tilton"
+                                            target="_blank"
+                                            style={{textDecoration: 'underline'}}
+                                            title="переход на last.fm"
+                                            rel="noopener noreferrer">last.fm </a>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div
+                        className={toggleState === 2 ? "content  active-content" : "content"}
+                    >
+                        <div>
+                            <div className={"Parent"}>
+                                <div className={"child1"}>
+                                    <h3>Дискография</h3>
+                                    <br/>
+                                    <div style={{border: '1px solid #FFFFFF', padding: '5px'}}>
+                                        Нажми по альбому, чтобы открыть список песен.
+                                        Нажми по песне, чтобы справа появился сам трек, текст песни (если он есть) и мой
+                                        перевод (если он есть).
+                                        <br/>
+                                        <img src={'https://cinni.net/images/creatures/sanrio/maru_jump_rope.gif'}
+                                             style={{display: 'block', margin: '0 auto'}} alt={''}/>
+                                    </div>
+                                    <br/>
+                                    <ul type="none">
+                                        <li>
+                                            <div onClick={handleClickShowSongs1} className={'albumList'}><img
+                                                src={require('./images/springtime.jpg')}
+                                                style={{width: "50px", margin: '5px'}} alt={''}/> ...Wake Me When It's Springtime
+                                                Again (1994) {open1 ? <FiChevronUp/> : <FiChevronDown/>}</div>
+                                            {isShownSongs1 && (
+                                                <ol className="rectangle">
+                                                    <div>
+                                                        {songsData.filter(
+                                                            item => (item.album || '').includes('1994')
+                                                        ).map(u => (
+                                                            <>
+                                                                <li key={u.key}
+                                                                    onClick={() => setSelectedSong(u)}>{u.name}</li>
+                                                            </>
+                                                        ))}
+                                                    </div>
+                                                </ol>
+                                            )}
+                                        </li>
+                                        <li>
+                                            <div onClick={handleClickShowSongs2} className={'albumList'}><img
+                                                src={require('./images/penknife.jpg')}
+                                                style={{width: "50px", margin: '5px'}} alt={''}/>Songs Of Penknife And Pocket
+                                                Watch (1995) {open2 ? <FiChevronUp/> : <FiChevronDown/>}</div>
+                                            {isShownSongs2 && (
+                                                <ol className="rectangle">
+                                                    <div>
+                                                        {songsData.filter(
+                                                            item => (item.album || '').includes('1995')
+                                                        ).map(u => (
+                                                            <>
+                                                                <li key={u.key}
+                                                                    onClick={() => setSelectedSong(u)}>{u.name}</li>
+                                                            </>
+                                                        ))}
+                                                    </div>
+                                                </ol>
+                                            )}
+                                        </li>
+                                        <li>
+                                            <div onClick={handleClickShowSongs3} className={'albumList'}><img
+                                                src={require('./images/crescent.jpg')}
+                                                style={{width: "50px", margin: '5px'}} alt={''}/>Crescent (1996) {open3 ?
+                                                <FiChevronUp/> : <FiChevronDown/>}</div>
+                                            {isShownSongs3 && (
+                                                <ol className="rectangle">
+                                                    <div>
+                                                        {songsData.filter(
+                                                            item => (item.album || '').includes('1996')
+                                                        ).map(u => (
+                                                            <>
+                                                                <li key={u.key}
+                                                                    onClick={() => setSelectedSong(u)}>{u.name}</li>
+                                                            </>
+                                                        ))}
+                                                    </div>
+                                                </ol>
+                                            )}
+                                        </li>
+                                        <li>
+                                            <div onClick={handleClickShowSongs4} className={'albumList'}><img
+                                                src={require('./images/hotels.jpg')}
+                                                style={{width: "50px", margin: '5px'}} alt={''}/>The Leading Hotels of the World
+                                                (1999) {open4 ? <FiChevronUp/> : <FiChevronDown/>}</div>
+                                            {isShownSongs4 && (
+                                                <ol className="rectangle">
+                                                    <div>
+                                                        {songsData.filter(
+                                                            item => (item.album || '').includes('1999')
+                                                        ).map(u => (
+                                                            <>
+                                                                <li key={u.key}
+                                                                    onClick={() => setSelectedSong(u)}>{u.name}</li>
+                                                            </>
+                                                        ))}
+                                                    </div>
+                                                </ol>
+                                            )}
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className={"child2"}>
+                                    {selectedSong ? <SongLyrics song={selectedSong}/> : null}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        className={toggleState === 3 ? "content  active-content" : "content"}
+                    >
+                        <h4 style={{textAlign: 'center', marginBottom: '2vh'}}>Bob Tilton - Live Nottingham - 8/1/1994</h4>
+                        <iframe className={'video'}
+                            src="https://www.youtube.com/embed/viAXz0CHBek"
+                            title="YouTube video player"
+                            frameBorder="0"
+                            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ emo-music-website/src/main-page/MainPage.js
@@ -0,0 +1,140 @@
+import React, {useState} from 'react'
+import '../styles/App.css';
+import '../styles/tabs.scss';
+import '../styles/list.css';
+import '../styles/columns.css';
+import '../styles/audio-video.css';
+import {BandsTable} from "../components/BandsTable";
+
+export function MainPage() {
+    const [toggleState, setToggleState] = useState(1);
+
+    const toggleTab = (index) => {
+        setToggleState(index);
+    };
+    return (
+        <div className="mainPage">
+            <div className={"empty"}/>
+            <h1> That's life: it's so social <br/>
+                So physical <br/>
+                So emotional <br/>
+                So stay home ... and listen to emo music <br/>
+            </h1>
+            <h5>(and sometimes go 2 gigs)</h5>
+            <div className="data">
+                <div className={"empty"}/>
+                <div className="tab-bar radius">
+                    <button className={toggleState === 1 ? "tab selected" : "tab"} onClick={() => toggleTab(1)}
+                       data-text="О группе">
+                        <span className="icon">О сайте</span>
+                    </button>
+                    <button className={toggleState === 2 ? "tab selected" : "tab"} onClick={() => toggleTab(2)}
+                       data-text="Дискография">
+                        <span className="icon">О жанре</span>
+                    </button>
+                    <button className={toggleState === 3 ? "tab selected" : "tab"} onClick={() => toggleTab(3)}
+                       data-text="Видосы">
+                        <span className="icon">Группы</span>
+                    </button>
+                    <button className={toggleState === 4 ? "tab selected" : "tab"} onClick={() => toggleTab(4)}
+                       data-text="Видосы">
+                        <span className="icon">Лейблы</span>
+                    </button>
+                </div>
+                <div className={"empty"}/>
+                <div className={"blackback"}>
+                    <div className="content-tabs">
+                        <div
+                            className={toggleState === 1 ? "content  active-content" : "content"}
+                        >
+                            Добро пожаловать в мой веб архив по эмо музыке. Идея создать такой сайт связана с тем, что
+                            мне нравится
+                            не просто слушать музыку, но так же и вникать в тексты и изучать историю групп. В этом месте
+                            собраны эмо группы, которые
+                            мне больше всего понравились и затронули сердце. Создавать странички для них немного
+                            времязатратный процесс, поэтому все будет появляться постепенно.
+                            Скажу сразу, я не занимаюсь фронтендом профессионально, поэтому возможно, кому-то интерфейс
+                            покажется всратым, но это здесь не главное :)
+                            <br/><br/>
+                            <img src={'https://dannarchy.com/misc/anim/myskull.gif'}
+                                 style={{display: 'block', margin: '0 auto'}} alt={''}/>
+                            <div>Если есть какие-то пожелания (добавить какие-то группы) или у вас есть крутая инфа или
+                                видосы,
+                                да или если вы просто нашли баги, то можете написать мне:
+                                @floralunit
+                            </div>
+                        </div>
+                        <div
+                            className={toggleState === 2 ? "content  active-content" : "content"}
+                        >
+                            <div>
+                                <h3> О жанре</h3>
+                                <b> Эмо </b>(англ. emo, сокращение от «эмоциональный») — стиль рок-музыки,
+                                характеризуемый мелодичной
+                                музыкальностью и экспрессивной лирикой. <br/>
+                                Появился в середине 1980-х в Вашингтоне, где был известен как «эмоциональный
+                                хардкор-панк» или
+                                «эмокор», разработанный такими группами, как Rites of Spring и Embrace.
+                                Стиль был переработан американскими панк-рокерами в начале 1990-х, его звучание
+                                изменилось, смешалось с
+                                поп-панком и инди-роком, подобно звучанию групп Jawbreaker и Sunny Day Real Estate.
+                                <br/>
+
+                                К середине 1990-х многочисленные эмо-группы появились на Среднем Западе и в центральной
+                                части США, а
+                                также несколько инди-лейблов стали специализироваться в данном стиле. Эмо ворвался в
+                                мейнстрим в начале 2000-х,
+                                с успехом групп Jimmy Eat World и Dashboard Confessional и появлением поджанра скримо.
+                                <br/>
+
+                                На базе поклонников эмо и родственных жанров возникла одноимённая субкультура.
+                                <h3>Мидвест-эмо</h3>
+                                <p>
+                                    Мидвест-эмо или инди-эмо — поджанр эмо-музыки, который так же называют "эмо второй
+                                    волны" отличающийся меланхоличным звучанием, а также контрастными переходами от
+                                    мягкого к громкому звучанию. Мидвест получился путём смешения инди, пост-гранжа и
+                                    постхардкора.
+                                    <br/>К пионерам жанра можно отнести такие группы, как Sunny Day Real Estate, The
+                                    Promise Ring, Mineral[1] и другие эмо-группы 90-х.
+                                </p>
+                                <h3>История</h3>
+                                <p>
+                                    Вслед за успехом альбома Nevermind (1991) группы Nirvana, андеграундная рок-музыка в
+                                    Соединённых Штатах превратилась в один большой бизнес. Появились новые
+                                    дистрибьюторские сети, а различные инди-группы получили доступ к национальному
+                                    уровню. Подростки по всей стране объявили себя поклонниками независимой музыки, быть
+                                    панком и приверженцем sXe стало модной тенденцией. В этом новом музыкальном климате,
+                                    эстетика эмо расширилась в сторону мейнстрима. Поскольку Sunny Day Real Estate
+                                    фактически были основателями жанра, основное признание и популярность досталась
+                                    им.[2]
+                                </p>
+                            </div>
+                        </div>
+
+                        <div
+                            className={toggleState === 3 ? "content  active-content" : "content"}
+                        >
+                            <h4 style={{textAlign: 'center', marginBottom: '2vh'}}>Список будет пополняться...</h4>
+                            <BandsTable/>
+                        </div>
+
+                        <div
+                            className={toggleState === 4 ? "content  active-content" : "content"}
+                        >
+                            <h3>Лейблы</h3>
+                            <div>Пока пусто...</div>
+                            <iframe className={'video'}
+                                    src="https://www.youtube.com/embed/J---aiyznGQ?autoplay=1"
+                                    title="YouTube video player"
+                                    frameBorder="0"
+                                    allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen>
+                            </iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
