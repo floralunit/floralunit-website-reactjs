e2f25d110d847b26628389f7d8e39208f90ad2b4
floralunit <anya710342@gmail.com>
2023-08-10 22:07:41 +0300

    исправлена проблема с качеством заголовка

--- WitchHouseMusic/src/main-page/BloodText.js
+++ WitchHouseMusic/src/main-page/BloodText.js
@@ -1,36 +1,29 @@
 import React, { useEffect, useRef } from 'react';
 import './BloodText.css';
-export class BloodText extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            componentSize: {width: props.width,height: props.height}
-        }
-    }
-    render(){
-        return (
-            <div className="bloodText">
-                <svg version="1.1" viewBox="0 0 0 0" width="0" height="0"
-                     style={{display:'none'}}>
-                    <defs>
-                        <filter id="goo">
-                            <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur"/>
-                            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
-                                           result="goo"/>
-                        </filter>
-                    </defs>
-                </svg>
 
-                <div className="goo">
-                    <h1>WitchHouse</h1>
-                    <div className="drop"></div>
-                    <div className="drop"></div>
-                    <div className="drop"></div>
-                    <div className="drop"></div>
-                    <div className="drop"></div>
-                    <div className="drop"></div>
-                </div>
+export function BloodText() {
+    return (
+        <div className="bloodText">
+            <svg version="1.1" viewBox="0 0 0 0" width="0" height="0"
+                 style={{display:'none'}}>
+                <defs>
+                    <filter id="goo">
+                        <feGaussianBlur in="SourceGraphic" stdDeviation="4" result="blur"/>
+                        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+                                       result="goo"/>
+                    </filter>
+                </defs>
+            </svg>
+
+            <div className="goo">
+                <h1>𝙒𝙞𝙩𝙘𝙝 𝙃𝙤𝙪𝙨𝙚</h1>
+                <div className="drop"></div>
+                <div className="drop"></div>
+                <div className="drop"></div>
+                <div className="drop"></div>
+                <div className="drop"></div>
+                <div className="drop"></div>
             </div>
-        );
-    }
+        </div>
+    );
 }
--- WitchHouseMusic/src/main-page/MainPage.js
+++ WitchHouseMusic/src/main-page/MainPage.js
@@ -9,16 +9,7 @@ export function MainPage() {
     return (
         <div>
             <MovingText />
-            <BloodText height="50px"/>
-            <MovingText />
-            <div className={"empty"}/>
-            <h1> That's life: it's so social <br/>
-                So physical <br/>
-                So emotional <br/>
-                So stay home ... and listen to emo music <br/>
-            </h1>
-            <h5>(and sometimes go 2 gigs)</h5>
-            <br/><br/>
+            <BloodText/>
         </div>
     );
 }
