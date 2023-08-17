e706fd153acf33e9e62b0f3bac6dc77257790981
floralunit <anya710342@gmail.com>
2023-08-18 02:21:39 +0300

    небольшие правки и сделан css для секции с испольнителями

--- WitchHouseMusic/src/index.js
+++ WitchHouseMusic/src/index.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/index.css';
-import './fonts/Rubik_Wet_Paint/RubikWetPaint-Regular.ttf';
-import './fonts/Handjet/static/Handjet-Medium.ttf';
-import './fonts/deadwax-font/DeadwaxDemoHard-gxadp.ttf';
-import './fonts/evil-sound-font/EvilSound-nRM8M.ttf';
-import './fonts/jatmika-font/JatmikaTypeface-2OYlK.otf';
-import './fonts/kings-and-pirates-font/KingsAndPirates-p7EAK.ttf';
-import './fonts/treasure-map-font/Treasuremap-Ea1vj.ttf';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- WitchHouseMusic/src/main-page/MainPage.js
+++ WitchHouseMusic/src/main-page/MainPage.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import './MainPage.css';
-import {Thissadnesswilllastforever} from "./thissadnesswilllastforever";
+import '../styles/photos.css';
+import {Thissadnesswilllastforever} from "../components/thissadnesswilllastforever";
 import {BloodText} from "../components/BloodText";
 import GlitchClip from 'react-glitch-effect/core/GlitchClip';
 import GlitchSquiggly from 'react-glitch-effect/core/GlitchSquiggly';
@@ -16,74 +17,71 @@ export function MainPage() {
     return (
         <div>
             <WebampMusic/>
-            <Thissadnesswilllastforever/>
-            <BloodText/>
                 <section className="info-section">
                     <div  className="container">
-                        <div className="info-text">
-                            Witch house (sometimes referred to as drag)[1] is a microgenre of electronic music that is musically characterized by high-pitched keyboard effects, heavily layered basslines and trap-style drum loops, while it aesthetically employs occult and gothic-inspired themes.[2][3]
-
-                            The witch house visual aesthetic includes occultism, witchcraft, shamanism, terror and horror-inspired artworks, collages and photographs as well as significant use of hidden messages and typographic elements such as Unicode symbols.[4][5] Artworks by witch house visual artists have incorporated imagery from horror films such as The Blair Witch Project,[6][dead link] the television series Twin Peaks,[7] and the fantasy show Charmed,[8][9] as well as mainstream pop culture celebrities of the 2000s. Common typographic elements in titles, such as by Salem and White Ring, include triangles, crosses and Unicode symbols, which are seen by some as a method of gatekeeping (in an effort to keep the scene underground and more difficult to search for on the Internet).
+                        <div className="head-container">
+                            <hr/>
+                            <Thissadnesswilllastforever/>
+                            <hr/>
+                            <BloodText/>
                         </div>
-                        <hr/>
-                        <div className="vv17chContainer">
-                            <div>
-                                <div className="vv17ch">
-                                    <img src={require('./resources/vedmindom.png')} loading="lazy" className="vv17chImg"/>
-                                    <span className="vv17chCaption">Ведьмин дом</span>
-                                </div>
-                                <div className="vv17ch">
-                                    <img src={require('./resources/vv17chou7.jpg')} loading="lazy" className="vv17chImg"/>
-                                    <span className="vv17chCaption">vv17chou7</span>
+                        <div className="info-container">
+                            <div className="info-text">
+                               Witch house is an occult-themed dark electronic music genre and visual aesthetic that emerged in the early 2010s.
+                                The music is heavily influenced by chopped and screwed hip-hop soundscapes, industrial and noise experimentation, and features use of synthesizers, drum machines, obscure samples, droning repetition and heavily altered, ethereal, indiscernible vocals.
+                                <br/><br/>   The witch house visual aesthetic includes occult, witchcraft, shamanism and horror-inspired artworks, collages and photographs as well as significant use of typographic elements such as Unicode symbols. Many works by witch house visual artists incorporate themes from horror films, the television series Twin Peaks, and mainstream pop culture celebrities. Common typographic elements in artist and track names include triangles, crosses, and other Unicode symbols, which are seen by some as a method of keeping the scene underground and harder to search for on the Internet.
+                            </div>
+                            <hr/>
+                            <div className="vv17chContainer">
+                                <div>
+                                    <div className="vv17ch">
+                                        <img src={require('./resources/angel-right.png')} loading="lazy" className="vv17chImg"/>
+                                    </div>
+                                    <div className="vv17ch">
+                                        <img src={require('./resources/vedmindom.png')} loading="lazy" className="vv17chImg"/>
+                                        <a className="vv17chCaption">Ведьмин дом</a>
+                                    </div>
+                                    <div className="vv17ch">
+                                        <img src={require('./resources/vv17chou7.jpg')} loading="lazy" className="vv17chImg"/>
+                                        <a className="vv17chCaption">vv17chou7</a>
+                                    </div>
+                                    <div className="vv17ch">
+                                        <img src={require('./resources/angel-left.png')} loading="lazy" className="vv17chImg"/>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </section>
-            <div className="name">
-                <Link to="/radostmoja">
-                    <GlitchClip onHover={false} className="s">
-                        <img src={require('./resources/radostmoja.png')} loading="lazy" className="s"/>
-                    </GlitchClip>
-                </Link>
-            <img src={require('./resources/2015_01.jpg')} loading="lazy" className="l"/>
-            <img src={require('./resources/2015_01.jpg')} loading="lazy" className="l"/>
-            <img src={require('./resources/2015_01.jpg')} loading="lazy" className="p"/>
-            <img src={require('./resources/2015_01.jpg')} loading="lazy" className="p"/>
-            <img src={require('./resources/2015_01.jpg')} loading="lazy" className="s"/>
-            <img src={require('./resources/2015_01.jpg')} loading="lazy" className="l"/>
-            <img src={require('./resources/2015_01.jpg')} loading="lazy" className="l"/>
-            <img src={require('./resources/2015_01.jpg')} loading="lazy" className="l"/>
-            <img src={require('./resources/2015_01.jpg')} loading="lazy" className="l"/>
-            <img src={require('./resources/2015_01.jpg')} loading="lazy" className="s"/>
-
-            <h2>2019</h2>
-
-            <img src={require('./resources/2015_01.jpg')} loading="lazy" className="l"/>
-            <img src={require('./resources/2015_01.jpg')} loading="lazy" className="l"/>
-            <img src={require('./resources/2015_01.jpg')} loading="lazy" className="s"/>
-            <img src={require('./resources/2015_01.jpg')} loading="lazy"
-                 className="l"/>
-            <img src={require('./resources/2015_01.jpg')} loading="lazy"
-                 className="l"/>
-            <img src={require('./resources/2015_01.jpg')} loading="lazy"
-                 className="l"/>
-            <img src={require('./resources/2015_01.jpg')}
-                 loading="lazy" className="p"/>
-                <img src={require('./resources/2015_01.jpg')}
-                     loading="lazy"
-                     className="l"/>
-                <img src={require('./resources/2015_01.jpg')}
-                     loading="lazy"
-                     className="l"/>
-                <img src={require('./resources/2015_01.jpg')}
-                     loading="lazy"
-                     className="l"/>
-                <img
-                    src={require('./resources/2015_01.jpg')}
-                    loading="lazy"
-                    className="p"/>
-            </div>
+            <section className="music-section">
+                <div  className="container">
+                    <div className="music-container">
+                        <div className="row">
+                            <div className="column">
+                                <Link to="/radostmoja">
+                                    <GlitchClip onHover={false} className="s">
+                                        <img src={require('./resources/radostmoja.png')} loading="lazy" className="s"/>
+                                    </GlitchClip>
+                                </Link>
+                            </div>
+                            <div className="column">
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <Link to="/suicidewave">
+                                        <div style={{fontSize: "100px"}}>SUICIDEWAVE</div>
+                                </Link>
+                            </div>
+                            <div className="column">
+                                <Link to="/suicidewave">
+                                    <div style={{fontSize: "100px"}}>CAIN</div>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
 );
 }
