061961eeacb2166613eb834adb707f75e700af7a
floralunit <anya71034@gmail.com>
2024-03-24 00:42:01 +0300

    добавлена страница с видео

--- src/main-website/components/updates-list.js
+++ src/main-website/components/updates-list.js
@@ -1,37 +1,42 @@
-export const updates = [{
-    id: 8,
-    date: '2024-03-23',
-    note: 'Create bgs, icons page',
-},{
-    id: 7,
-    date: '2024-03-21',
-    note: 'Create games page',
-},{
-    id: 6,
-    date: '2024-03-19',
-    note: 'Finished with photo pages',
-},{
-    id: 5,
-    date: '2024-01-09',
-    note: 'Working on photos pages, add api functionality',
-}, {
-    id: 4,
-    date: '2023-09-04',
-    note: 'End main template of site, add side components and info page',
-}, {
-    id: 3,
-    date: '2023-08-19',
-    note: 'Create united project and start main site',
-}, {
-    id: 2,
-    date: '2023-08-10',
-    note: 'Start working on witch house page',
-}, {
-    id: 1,
-    date: '2022-08-04',
-    note: 'End emo main page and create first band page',
-}, {
-    id: 0,
-    date: '2022-08-02',
-    note: 'Start working on emo page',
-}];
+export const updates = [
+    {
+        id: 9,
+        date: '2024-03-24',
+        note: 'Create video gallery page',
+    }, {
+        id: 8,
+        date: '2024-03-23',
+        note: 'Create bgs, icons page',
+    }, {
+        id: 7,
+        date: '2024-03-21',
+        note: 'Create games page',
+    }, {
+        id: 6,
+        date: '2024-03-19',
+        note: 'Finished with photo pages',
+    }, {
+        id: 5,
+        date: '2024-01-09',
+        note: 'Working on photos pages, add api functionality',
+    }, {
+        id: 4,
+        date: '2023-09-04',
+        note: 'End main template of site, add side components and info page',
+    }, {
+        id: 3,
+        date: '2023-08-19',
+        note: 'Create united project and start main site',
+    }, {
+        id: 2,
+        date: '2023-08-10',
+        note: 'Start working on witch house page',
+    }, {
+        id: 1,
+        date: '2022-08-04',
+        note: 'End emo main page and create first band page',
+    }, {
+        id: 0,
+        date: '2022-08-02',
+        note: 'Start working on emo page',
+    }];
--- src/main-website/pages/Photos/VideosPage.js
+++ src/main-website/pages/Photos/VideosPage.js
@@ -1,15 +1,50 @@
+import { ANGEL_BACKGROUND, CLOTH_BACKGROUND, MARBLE1_BACKGROUND } from "../../../global-const";
 import "../../MainWebsite.css"
 
 import React, { useEffect, useState } from 'react'
 
 export function VideosPage() {
-    document.documentElement.style.removeProperty('--box-header-color');
-    document.documentElement.style.removeProperty('--main-background');
-    document.documentElement.style.removeProperty('--box-background');
     document.documentElement.style.removeProperty('--box-text-color');
+    document.documentElement.style.setProperty('--box-header-color', `#3d2b1f`);
+    document.documentElement.style.setProperty('--main-background', `url(${ANGEL_BACKGROUND})`);
+    document.documentElement.style.setProperty('--box-background', `url(${CLOTH_BACKGROUND})`);
     return (
-        <div className='main' style={{ maxWidth: "650px", fontSize: 'smaller' }}>
- 
+        <div className='main' style={{ maxWidth: "700px", fontSize: 'smaller' }}>
+                    <div className="box08">
+          <div className="box-top">
+            <div className="u01"></div>
+          </div>
+          <div className="box-center">
+            <div className="box-inner" style={{ textAlign: 'center', fontFamily: 'LCD5', fontSize: '1.5em' }}>
+              My video gallery
+              <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <img src={require('../../resources/tabs_dividers_pixels/zwlk1.gif')}
+                  style={{ height: '40px', width: 'auto', padding: '5px 0 0 0', alignSelf: 'center' }} />
+              </div>
+            </div>
+          </div>
+          <div className="box-bottom">
+            <div className="s01"></div>
+          </div>
+        </div>
+            <div className="box">
+                <div className="inner">
+                    <div style={{display: 'flex', flexWrap:'wrap', justifyContent:'space-evenly'}}>
+                    <iframe src="https://vk.com/video_ext.php?oid=283385573&id=456239670" width="270" height="152" style={{margin:'10px'}} allow="autoplay; encrypted-media; fullscreen; picture-in-picture;" frameborder="0" allowfullscreen></iframe>
+                        <iframe src="https://vk.com/video_ext.php?oid=283385573&id=456239669" width="270" height="152" style={{margin:'10px'}} allow="autoplay; encrypted-media; fullscreen; picture-in-picture;" frameborder="0" allowfullscreen></iframe>
+                        <iframe src="https://vk.com/video_ext.php?oid=283385573&id=456239684" width="270" height="152" style={{margin:'10px'}} allow="autoplay; encrypted-media; fullscreen; picture-in-picture;" frameborder="0" allowfullscreen></iframe>
+                        <iframe src="https://vk.com/video_ext.php?oid=283385573&id=456239685" width="270" height="152" style={{margin:'10px'}} allow="autoplay; encrypted-media; fullscreen; picture-in-picture;" frameborder="0" allowfullscreen></iframe>
+                        <iframe src="https://vk.com/video_ext.php?oid=283385573&id=456239675" width="270" height="180" style={{margin:'10px'}} allow="autoplay; encrypted-media; fullscreen; picture-in-picture;" frameborder="0" allowfullscreen></iframe>
+                        <iframe src="https://vk.com/video_ext.php?oid=283385573&id=456239682" width="270" height="180" style={{margin:'10px'}} allow="autoplay; encrypted-media; fullscreen; picture-in-picture;" frameborder="0" allowfullscreen></iframe>
+                    </div>
+
+                    <div style={{ display: 'flex', justifyContent:'space-evenly'}}>
+                    <iframe src="https://vk.com/video_ext.php?oid=283385573&id=456239683" width="270" height="474" style={{margin:'10px'}} allow="autoplay; encrypted-media; fullscreen; picture-in-picture;" frameborder="0" allowfullscreen></iframe>
+                        <iframe src="https://vk.com/video_ext.php?oid=283385573&id=456239681" width="270" height="474" style={{margin:'10px'}} allow="autoplay; encrypted-media; fullscreen; picture-in-picture;" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 }
