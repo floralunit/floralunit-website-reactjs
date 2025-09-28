4b5f64c442d8d6039bc70615f51a20ab379d6da0
floralunit <anya71034@gmail.com>
2025-09-28 05:38:36 +0300

    отдельный компонент для дискрографии на эмо сайте, доработка json песен

--- src/emo-music-website/components/AlbumImages.js
+++ src/emo-music-website/components/AlbumImages.js
@@ -4,14 +4,14 @@ const albumImages = {
         '​И​ж​е С Н​а​м​и​ (2016)': require('../emo-bands/kashtanka/images/Иже с нами.jpg'),
         'В​з​а​и​м​н​о​с​т​ь​ (2018)': require('../emo-bands/kashtanka/images/Взаимность.jpg'),
         'П​р​о​щ​а​й​. В​е​р​н​и​с​ь (2020)': require('../emo-bands/kashtanka/images/Прощай Вернись.jpg'),
-        'В​е​ч​н​о​е л​е​т​о​ (Cover) (2020)': require('../emo-bands/kashtanka/images/Вечное лето.jpg'),
+        'В​е​ч​н​о​е л​е​т​о​ (Cover) (2020)': require('../emo-bands/kashtanka/images/Вечное лето.jpg')
     },
     '30 Дней Февраля': {
         'Твоя сестра Саши (2005)': require('../emo-bands/30-dnej-fevralya/images/Твоя сестра Саши.jpg'),
-        'Singles/Demo': require('../emo-bands/30-dnej-fevralya/images/demo.jpg'),
+        'Singles/Demo': require('../emo-bands/30-dnej-fevralya/images/demo.jpg')
     },
     '6000000000себя': {
-        'Прошу, убей меня! (2008)': require('../emo-bands/6000000000sebya/images/прошу_убей_меня.jpg'),
+        'Прошу, убей меня! (2008)': require('../emo-bands/6000000000sebya/images/прошу_убей_меня.jpg')
     },
     'American Football': {
         'American Football (EP) (1998)': require('../emo-bands/americ-anfootball/images/American Football (EP).jpg'),
@@ -19,25 +19,25 @@ const albumImages = {
         'American Football (LP2) (2016)': require('../emo-bands/americ-anfootball/images/American Football (LP2).jpg'),
         'American Football (LP3) (2019)': require('../emo-bands/americ-anfootball/images/American Football (LP3).jpg'),
         'Year One Demos (2019)': require('../emo-bands/americ-anfootball/images/Year One Demos.jpg'),
-        'Rare Symmetry / Fade Into You (2021)': require('../emo-bands/americ-anfootball/images/Rare Symmetry.jpg'),
+        'Rare Symmetry / Fade Into You (2021)': require('../emo-bands/americ-anfootball/images/Rare Symmetry.jpg')
     },
     'asthenia': {
         'Four Songs (2014)': require('../emo-bands/asthenia/images/Four Songs.jpg'),
         'The City / Fun (2019)': require('../emo-bands/asthenia/images/The City Fun.webp'),
         'Split Tape with Ensslin (2021)': require('../emo-bands/asthenia/images/Split Tape with Ensslin.jpg'),
-        '9 Songs (2023)': require('../emo-bands/asthenia/images/9 Songs.jpg'),
+        '9 Songs (2023)': require('../emo-bands/asthenia/images/9 Songs.jpg')
     },
     'Bob Tilton': {
         "...Wake Me When It's Springtime Again (1994)": require('../emo-bands/bob-tilton/images/springtime.jpg'),
         "Songs Of Penknife And Pocket Watch (1995)": require('../emo-bands/bob-tilton/images/penknife.jpg'),
         "Crescent (1996)": require('../emo-bands/bob-tilton/images/crescent.jpg'),
-        "The Leading Hotels of the World (1999)": require('../emo-bands/bob-tilton/images/hotels.jpg'),
+        "The Leading Hotels of the World (1999)": require('../emo-bands/bob-tilton/images/hotels.jpg')
     },
     'botanichesky sad': {
         "Self​-​Titled (2017)": require('../emo-bands/botanichesky-sad/images/Self​-​Titled.jpg'),
         "Т​р​е​в​о​г​а (single) (2018)": require('../emo-bands/botanichesky-sad/images/Т​р​е​в​о​г​а (single).jpg'),
         "с​е​г​о​д​н​я п​р​о​с​т​о не м​о​й д​е​н​ь (2019)": require('../emo-bands/botanichesky-sad/images/с​е​г​о​д​н​я п​р​о​с​т​о не м​о​й д​е​н​ь.jpg'),
-        "мы в​с​т​р​е​т​и​м​с​я т​ё​п​л​ы​м л​е​т​о​м (2019)": require('../emo-bands/botanichesky-sad/images/мы в​с​т​р​е​т​и​м​с​я т​ё​п​л​ы​м л​е​т​о​м.jpg'),
+        "мы в​с​т​р​е​т​и​м​с​я т​ё​п​л​ы​м л​е​т​о​м (2019)": require('../emo-bands/botanichesky-sad/images/мы в​с​т​р​е​т​и​м​с​я т​ё​п​л​ы​м л​е​т​о​м.jpg')
     },
     'Camping in Alaska': {
         "tuesday (featuring brian zaremba of exalted ruler 1998 on guitar) (2012)": require('../emo-bands/camping-in-alaska/images/tuesday.jpg'),
@@ -48,8 +48,95 @@ const albumImages = {
         "WELCOME HOME SON (2016)": require('../emo-bands/camping-in-alaska/images/WELCOME HOME SON.jpg'),
         "ICARUS (2019)": require('../emo-bands/camping-in-alaska/images/ICARUS.jpg'),
         "Lost and Found (2022)": require('../emo-bands/camping-in-alaska/images/Lost and Found.jpg'),
-        "HOLLOW EYES (2023)": require('../emo-bands/camping-in-alaska/images/HOLLOW EYES.jpg'),
+        "HOLLOW EYES (2023)": require('../emo-bands/camping-in-alaska/images/HOLLOW EYES.jpg')
     },
+    'Daïtro': {
+        "Laisser Vivre Les Squelettes (2005)": require('../emo-bands/daitro/images/Laisser Vivre Les Squelettes.jpg'),
+        "Des Cendres, Je Me Consume (2003)": require('../emo-bands/daitro/images/Des Cendres, Je Me Consume.jpg'),
+        "split w/ Raein (2004)": require('../emo-bands/daitro/images/split w Raein.jpg'),
+        "split w/ Ampere (2007)": require('../emo-bands/daitro/images/split w Ampere.jpg'),
+        "US tour 7 inch (2007)": require('../emo-bands/daitro/images/US tour 7 inch.jpg'),
+        "split w/ Sed Non Satiata (2007)": require('../emo-bands/daitro/images/split w Sed Non Satiata.jpg'),
+        "Y (2009)": require('../emo-bands/daitro/images/Y.jpg')
+    },
+    'EORA': {
+        "Мы н​и​к​о​г​д​а не б​ы​л​и с​ч​а​с​т​л​и​в​ы з​д​е​с​ь (2011)": require('../emo-bands/eora/images/Мы н​и​к​о​г​д​а не б​ы​л​и с​ч​а​с​т​л​и​в​ы з​д​е​с​ь.jpg'),
+        "П​о​с​л​е​д​с​т​в​и​я (2012)": require('../emo-bands/eora/images/П​о​с​л​е​д​с​т​в​и​я.jpg'),
+        "Н​а​г​р​а​д​а за в​е​р​н​о​с​т​ь (2013)": require('../emo-bands/eora/images/Н​а​г​р​а​д​а за в​е​р​н​о​с​т​ь.jpg'),
+        "Split w. Ravin, Elephant Opinions & H​ý​aena (2014)": require('../emo-bands/eora/images/Split w. Ravin, Elephant Opinions & H​ý​aena.jpg'),
+        "Split w. Yarn of Norna (2014)": require('../emo-bands/eora/images/Split w. Yarn of Norna.jpg'),
+        "В​с​ё​, ч​е​г​о я б​о​я​л​с​я (2015)": require('../emo-bands/eora/images/В​с​ё​, ч​е​г​о я б​о​я​л​с​я.jpg'),
+        "У​т​е​ш​е​н​и​е (2022)": require('../emo-bands/eora/images/У​т​е​ш​е​н​и​е.jpg'),
+        "Пальцы (2023)": require('../emo-bands/eora/images/пальцы.jpg')
+    },
+    'это': {
+        "Самый большой (2020)": require('../emo-bands/eto/images/Самый большой.jpg'),
+        "Что дальше? (2021)": require('../emo-bands/eto/images/Что дальше.jpg'),
+        "Окна (2023)": require('../emo-bands/eto/images/Окна.jpg')
+    },
+    'Everyone Asked About You': {
+        "Everyone Asked About You (1997)": require('../emo-bands/everyone-asked-about-you/images/Everyone Asked About You.jpg'),
+        "Singles (1998)": require('../emo-bands/everyone-asked-about-you/images/singles.jpg'),
+        "Sometimes Memory Fails Me Sometimes (1998)": require('../emo-bands/everyone-asked-about-you/images/Sometimes Memory Fails Me Sometimes.png'),
+        "Let's Be Enemies (1999)": require('../emo-bands/everyone-asked-about-you/images/Lets Be Enemies.jpg')
+    },
+    'I Hate Myself': {
+        "Ten Songs (1997)": require('../emo-bands/i-hate-myself/images/10songs.jpg'),
+        "Four Songs - EP (1997)": require('../emo-bands/i-hate-myself/images/4songs.jpg'),
+        "Two Songs (2000)": require('../emo-bands/i-hate-myself/images/2songs.jpg'),
+        "Three Songs (2005)": require('../emo-bands/i-hate-myself/images/3songs.jpg')
+    },
+    'Love Lost but Not Forgotten': {
+        "Love Lost But Not Forgotten (2000)": require('../emo-bands/love-lost-but-not-forgotten/images/Love Lost But Not Forgotten.jpg'),
+        "Upon the Right, I Saw a New Misery (2002)": require('../emo-bands/love-lost-but-not-forgotten/images/Upon the Right, I Saw a New Misery.jpg')
+    },
+    'Oakwood': {
+        "Oakwood (2013)": require('../emo-bands/oakwood/images/Oakwood.jpg'),
+        "Summer (2015)": require('../emo-bands/oakwood/images/Summer.jpg')
+    },
+    'Old Gray': {
+        "Demo (2011)": require('../emo-bands/old-gray/images/demo.jpg'),
+        "Do I Dare Disturb the Universe (2011)": require('../emo-bands/old-gray/images/Do I Dare Disturb the Universe.jpg'),
+        "Everything I Let Go & The Things I Refuse To (2012)": require('../emo-bands/old-gray/images/Everything I Let Go & The Things I Refuse To.jpg'),
+        "An Autobiography (2013)": require('../emo-bands/old-gray/images/An Autobiography.jpg'),
+        "slow burn (2016)": require('../emo-bands/old-gray/images/slow burn.jpg'),
+        "singles/split": require('../emo-bands/old-gray/images/singles.jpg')
+    },
+    'Orchid': {
+        "Pig Destroyer (1997)": require('../emo-bands/orchid/images/Pig Destroyer.jpg'),
+        "Encyclopedia Of American Traitors (1998)": require('../emo-bands/orchid/images/Encyclopedia Of American Traitors.jpg'),
+        "Chaos Is Me (1999)": require('../emo-bands/orchid/images/Chaos Is Me.png'),
+        "Dance Tonight! Revolution Tomorrow! (2000)": require('../emo-bands/orchid/images/DanceTonight.png'),
+        "The Red Scare (2000)": require('../emo-bands/orchid/images/The Red Scare.jpg'),
+        "CombatWoundedVeteran (2000)": require('../emo-bands/orchid/images/CombatWoundedVeteran.jpg'),
+        "Gatefold (2002)": require('../emo-bands/orchid/images/Gatefold.png'),
+        "Totality (2005)": require('../emo-bands/orchid/images/Totality.jpg')
+    },
+    'The Rabbit Theory': {
+        "Reflections On Good Behaviour (2006)": require('../emo-bands/rabbit-theory/images/Reflections On Good Behaviour.jpg'),
+        "Thanks, But No Thanks (2007)": require('../emo-bands/rabbit-theory/images/Thanks, But No Thanks.jpg'),
+        "Save / Cancel (2008)": require('../emo-bands/rabbit-theory/images/Save Cancel.jpg'),
+        "singles/splits (2012-2013)": require('../emo-bands/rabbit-theory/images/split cassingle with Mock (berlin).jpg')
+    },
+    'Reasons': {
+        "Hate Everything (2014)": require('../emo-bands/reasons/images/hate everything.jpg'),
+        "i promised i'd be okay, but when october comes i won't know what to do. (2014)": require('../emo-bands/reasons/images/i promise.png'),
+        ", (2015)": require('../emo-bands/reasons/images/,.jpg'),
+        "; (2016)": require('../emo-bands/reasons/images/;.jpg'),
+        "branapan (2017)": require('../emo-bands/reasons/images/branapan.jpg')
+    },
+    'Sleeping For Sunrise': {
+        "Sleeping For Sunrise (1997)": require('../emo-bands/sleeping-for-sunrise/images/Sleeping For Sunrise.jpg'),
+        "Skyline Symmetry (1998)": require('../emo-bands/sleeping-for-sunrise/images/Skyline Symmetry.jpg'),
+        "unreleased | Live - Mabel's, Champaign, Il (1998)": require('../emo-bands/sleeping-for-sunrise/images/unreleased.png')
+    },
+    'Strictly Ballroom': {
+        "Strictly Ballroom (1995)": require('../emo-bands/strictly-ballroom/images/Strictly Ballroom.jpg'),
+        "7'' (1996)": require('../emo-bands/strictly-ballroom/images/7.jpg'),
+        "Hide Here Forever (1997)": require('../emo-bands/strictly-ballroom/images/Hide Here Forever.jpg')
+    }
+
+
 };
 
 export default albumImages;
